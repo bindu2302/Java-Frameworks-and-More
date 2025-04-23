560d4dafb335a87321bdea1ad9b77cb59e9d165d
HimaBindu Nara <naarahimabindu@gmail.com>
2025-04-23 22:53:33 +0530

    Update App.java

--- hiber/src/main/java/learn/hiber/App.java
+++ hiber/src/main/java/learn/hiber/App.java
@@ -1,16 +1,25 @@
 package learn.hiber;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 public class App 
 {
     public static void main( String[] args )
     {
+//    	Student st = new Student(4,"anitha","btn",2025);
     	Configuration cfg = new Configuration();
     	cfg.configure();
-    	SessionFactory fact =cfg.buildSessionFactory();
+    	SessionFactory factory =cfg.buildSessionFactory();
+    	Session sess = factory.openSession();
+//    	Transaction tx = sess.beginTransaction();
+//    	
+//    	sess.save(st);
+//    	tx.commit();
     	
-    	System.out.println(fact);
+    	Student st = sess.get(Student.class,5);
+    	System.out.println(st);
     }
 }
