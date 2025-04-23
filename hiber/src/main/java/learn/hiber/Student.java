6539dcfce0331fc0752db1ea3682362ab95fd55c
HimaBindu Nara <naarahimabindu@gmail.com>
2025-04-23 22:53:54 +0530

    Update Student.java

--- hiber/src/main/java/learn/hiber/Student.java
+++ hiber/src/main/java/learn/hiber/Student.java
@@ -10,12 +10,28 @@ public class Student {
 	String name;
 	String address;
 	int yop;
+	
+	public Student() {
+		super();
+	}
 		
 	public Student(int kodId,String name, String address, int yop) {
 		this.kodId = kodId;
 		this.name = name;
 		this.address = address;
 		this.yop = yop;
+	}
+	
+	public int getKodId() {
+		return kodId;
+	}
+	
+	public void setKodId(int kodId) {
+		this.kodId = kodId;
+	}
+	
+	public String getName() {
+		return name;
 	}
 
 }
