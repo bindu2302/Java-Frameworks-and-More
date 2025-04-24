2083d31376ba9637506e66ea40c0403a920c6f92
HimaBindu Nara <naarahimabindu@gmail.com>
2025-04-24 16:14:08 +0530

    Update Student.java

--- hiber/src/main/java/learn/hiber/Student.java
+++ hiber/src/main/java/learn/hiber/Student.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Id;
 
 @Entity
 public class Student {
+	
 	@Id
 	int kodId;
 	String name;
@@ -22,16 +23,43 @@ public class Student {
 		this.yop = yop;
 	}
 	
+
+	
 	public int getKodId() {
 		return kodId;
 	}
-	
+
 	public void setKodId(int kodId) {
 		this.kodId = kodId;
 	}
-	
+
 	public String getName() {
 		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public int getYop() {
+		return yop;
+	}
+
+	public void setYop(int yop) {
+		this.yop = yop;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [kodId=" + kodId + ", name=" + name + ", address=" + address + ", yop=" + yop + "]";
 	}
 
 }
