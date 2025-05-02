d927679b28e3abbb76c614553241572591ce9873
HimaBindu Nara <naarahimabindu@gmail.com>
2025-05-02 12:34:58 +0530

    Add files via upload

--- com.login/src/main/java/com/login/LoginServlet.java
+++ com.login/src/main/java/com/login/LoginServlet.java
@@ -2,7 +2,10 @@ package com.login;
 
 
 import java.io.IOException;
-import jakarta.servlet.annotation.WebServlet;
+import java.time.LocalDate;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -12,18 +15,31 @@ import jakarta.servlet.http.HttpServletResponse;
 //@WebServlet("/login")
 public class LoginServlet extends HttpServlet {
 	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-	    String dbUname = "virat";
-	    String dbPass = "1818";
-	    
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+//	    String dbUname = "virat";
+//	    String dbPass = "1818";
+		
 	    String webUname = request.getParameter("username");
 	    String webPass = request.getParameter("password");
 	    
-	    if(dbUname.equals(webUname) && dbPass.equals(webPass)) {
-	        response.getWriter().println("Login success");
-	    } else {
-	        response.getWriter().println("Login fail");
-	    }
+	    LocalDate time = LocalDate.now();
+	    request.setAttribute("time", time);
+	    request.setAttribute("username", webUname);
+	    RequestDispatcher rd = request.getRequestDispatcher("/home.jsp");
+	    rd.forward(request, response);
+	    
+	    
+//	    if(dbUname.equals(webUname) && dbPass.equals(webPass)) {
+////	    	RequestDispatcher rd = request.getRequestDispatcher("/success.html");
+////	    	rd.forward(request, response);
+	    // or
+//	    	response.sendRedirect("success.html");
+//	    } else {
+////	    	RequestDispatcher rd = request.getRequestDispatcher("/fail.html");
+////	    	rd.forward(request, response);
+	    // or
+//	    	response.sendRedirect("fail.html");
+//	    	}
 	}
 
 }
