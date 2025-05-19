c70bf89d85e4d8b68f3bb3544d6fd789f766874b
HimaBindu Nara <naarahimabindu@gmail.com>
2025-05-19 12:25:41 +0530

    Add files via upload

--- Postman-demo/src/main/java/com/example/demo/controller/NavController.java
+++ Postman-demo/src/main/java/com/example/demo/controller/NavController.java
@@ -1,8 +1,12 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entities.Student;
 
 @RestController
 public class NavController {
@@ -13,9 +17,15 @@ public class NavController {
 		return "Have a Good Day!";
 	}
 		
-		@GetMapping("/acceptData")
-	    public String acceptData(@RequestParam String data) {
+		@GetMapping("/manageData")
+	    public String manageData(@RequestParam String data) {
 	        System.out.println("Received data: " + data);
 	        return "Data received: " + data;
 	    }
+		
+		
+		@PostMapping("/manageObject")
+		public Student manageObject(@RequestBody Student st) {
+			return st;
+		}
 }
