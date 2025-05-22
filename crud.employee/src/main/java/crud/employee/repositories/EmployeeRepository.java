1be7b3ec33d9679e324aa964ebab85404a5baabf
HimaBindu Nara <naarahimabindu@gmail.com>
2025-05-22 11:08:45 +0530

    Delete crud.employee directory

--- crud.employee/src/main/java/crud/employee/repositories/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package crud.employee.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import crud.employee.entities.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
-
-}
--- crud.employee/src/main/java/crud/employee/services/EmployeeService.java
+++
@@ -1,8 +0,0 @@
-package crud.employee.services;
-
-import crud.employee.entities.Employee;
-
-public interface EmployeeService {
-	
-	void addEmployee(Employee emp);
-}
