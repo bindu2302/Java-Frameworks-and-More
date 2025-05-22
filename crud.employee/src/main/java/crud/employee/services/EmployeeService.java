e59bc35506b037b84d9eee79d074365dff04c2b8
HimaBindu Nara <naarahimabindu@gmail.com>
2025-05-22 11:09:46 +0530

    Add files via upload

---
+++ crud.employee/src/main/java/crud/employee/services/EmployeeService.java
@@ -0,0 +1,19 @@
+package crud.employee.services;
+
+import java.util.List;
+
+import crud.employee.entities.Employee;
+
+public interface EmployeeService {
+	
+	void addEmployee(Employee emp);
+	
+	void updateEmployee(Employee emp);
+	
+	Employee searchEmployee(int id);
+	
+	void deleteEmployee(int id);
+	
+	List<Employee> fetchAllEmployees();
+
+}
