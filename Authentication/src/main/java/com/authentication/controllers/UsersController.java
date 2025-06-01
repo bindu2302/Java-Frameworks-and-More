ea43bb4e0b728c6c04eecdcc8eb5ae5562ce0a35
HimaBindu Nara <naarahimabindu@gmail.com>
2025-06-01 18:26:57 +0530

    Add files via upload

--- Authentication/src/main/java/com/authentication/controllers/UsersController.java
+++ Authentication/src/main/java/com/authentication/controllers/UsersController.java
@@ -2,6 +2,7 @@ package com.authentication.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,25 +29,31 @@ public class UsersController {
 	
 	
 	@PostMapping("/signUp")
-	public String signUp(@ModelAttribute Users user) {
+	public String signUp(@ModelAttribute Users user, Model m) {
 		String username = user.getUsername();
 		boolean usernameExist = service.usernameExist(username);
-		if(usernameExist == false) 
+		
+		if(usernameExist == false) {
 			service.signUp(user);
-		else
-			System.out.println("User already exist!");
-		return "index";
+			m.addAttribute("msg","User registered successfully!");
+			return "sign_in";
+		}	
+		else {
+			m.addAttribute("msg","User already exist!");
+		    return "sign_up";
+		}
 	}
 	
 	@PostMapping("/signIn")
-	public String signIn(@ModelAttribute UserLoginData data) {
+	public String signIn(@ModelAttribute UserLoginData data, Model m) {
 		
 		Users dbUser = service.getUser(data.getUsername());
 		if(dbUser != null) {
 			if(data.getPassword().equals(dbUser.getPassword()))
 				return "success";
 		}
-		return "fail";
+		m.addAttribute("errorMsg","Wrong credentails, try again!");
+	    return "sign_in";
 	}
 	
 }
